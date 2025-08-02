e3b891dbf5d7efc8efc9a010362cb7c59a3cb0eb
zoystikwar-alt <zoystikwar@gmail.com>
2025-08-02 22:54:17 +0800

    pesan
    Anonim

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
+export const app = initializeApp({
+  apiKey: "AIzaSyDyVqF5Mmn0ImQ8V9JORBK-K0C2Bn-xBd4",
+  authDomain: "pesananonim-843d6.firebaseapp.com",
+  databaseURL: "https://pesananonim-843d6-default-rtdb.firebaseio.com",
+  projectId: "pesananonim-843d6",
+  storageBucket: "pesananonim-843d6.firebasestorage.app",
+  messagingSenderId: "628233179263",
+  appId: "1:628233179263:web:54b915aef951ae06a86f4a",
+  measurementId: "G-T002ZGE4MM"
+});
